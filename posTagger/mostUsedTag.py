4cf514781b3d9cf80e83d434438545a352ffd217
T. C <comelli.tom@gmail.com>
2021-04-21 19:00:02 +0200

    fixed most used tag

--- posTagger/mostUsedTag.py
+++ posTagger/mostUsedTag.py
@@ -1,9 +1,9 @@
 import pyconll
-from countpos import count_npos
 from pathlib import Path
 
 
-arrow = " -> "
+slash = " / "
+countname = {}
 cprob = {}
 frequent_tag_word = {}
 
@@ -12,14 +12,25 @@ file_path = Path("C:/Users/funkt/Documents/GitHub/nlp-greek-latin-pos-tagger/UD_
 corpus = pyconll.iter_from_file(file_path)
 corpus1 = pyconll.iter_from_file(file_path)
 
+#conta le frequenze dei nomi
+def count_name(corpus):
+    for sentence in corpus:
+        for token in sentence:
+            name = token.form
+            #se non c'è un name lo aggiunge nelle key di eprob e lo inizializza a 1, altrimenti aumenta di 1 il val del pos
+            if name not in countname.keys():
+                countname.update({name : 1})
+            else:
+                countname[name] += 1
+
 
 def most_used_tag(corpus):
-    # la prima parte della funzione calcola le occorrenze delle varie combinazioni parola -> tag
+    # la prima parte della funzione calcola le occorrenze delle varie combinazioni parola / tag
 
     for sentence in corpus:
         for token in sentence:
 
-            name = token.form + arrow + token.upos
+            name = token.form + slash + token.upos
 
             # se non c'è un name lo aggiunge nelle key di cprob e lo inizializza a 1, altrimenti aumenta di 1 il val del pos
             if name not in cprob.keys():
@@ -27,28 +38,25 @@ def most_used_tag(corpus):
             else:
                 cprob[name] += 1
 
-    # divide i value di cprob per il corrispettivo value di countpos, caratterizzato dallo stesso tag
+    # divide i value di cprob per il corrispettivo value di countname, cioè con lo stesso name
     for name in cprob:
-        # salva in una str la seconda parte della key di cprob cioè il tag
-        key_pos = name.split(arrow, 1)[1]
-        if key_pos in countpos.keys():
-            cprob[name] /= countpos[key_pos]
+        # salva in una str la prima parte della key di cprob cioè il nome
+        key_name = name.split(slash, 1)[0]
+        if key_name in countname.keys():
+            cprob[name] /= countname[key_name]
 
+    copy_dict = cprob.copy()
     #trova la parola con il tag più frequente per una ciascuna parola
     for name in cprob:
-        key_pos = name.split(arrow, 1)[0]
-        if key_pos not in frequent_tag_word:
-            frequent_tag_word.update({key_pos: cprob[name]})
-
-        else:
-            if cprob[name] > frequent_tag_word[key_pos]:
-                frequent_tag_word.update({key_pos : cprob[name]})
+        key_name = name.split(slash, 1)[0]
+        key_pos = name.split(slash, 1)[1]
+        for copy in copy_dict:
+            key_copy_name = copy.split(slash, 1)[0]
+            if key_copy_name == key_name and cprob[name] >= copy_dict[copy]:
+                frequent_tag_word.update({key_name : key_pos})
 
     return frequent_tag_word
 
-countpos = count_npos(corpus)
+count_name(corpus)
 freq_tag = most_used_tag(corpus1)
-
-print(countpos)
-print(cprob)
 print(frequent_tag_word)
