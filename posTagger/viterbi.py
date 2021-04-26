59c497f3ddd2b0850cb692937234a6e0ed4e67e7
T. C <comelli.tom@gmail.com>
2021-04-26 15:31:19 +0200

    Update viterbi.py

--- posTagger/viterbi.py
+++ posTagger/viterbi.py
@@ -35,38 +35,32 @@ def viterbi(corpus):
             token_arr[count] = token.form
 
         token_arr[count + 1] = eos
-        row_length = pos_len
-        print(token_arr)
 
         for col in range(token_arr.__len__()):
             for row in range(pos_len):
                 #per la prima colonna salva nella matrice tutte le probabilità di emissione (non calcolo quella di transizione perché nella prima colonna non c'è)
                 if col == 0:
-                    print(pos_array[row] + token_arr[col])
                     e_prob = read_files.get_eprob(pos_array[row], token_arr[col])
-                    mat[row, col] = e_prob
+                    mat[col, row] = e_prob
                 #per le successive colonne controllo le righe della colonna precedente e salvo la massima prob per ogni casella della colonna attuale
                 else:
                     col_prev = col - 1
                     e_prob = read_files.get_eprob(pos_array[row], token_arr[col])
-                    print(e_prob)
                     if e_prob == 0:
                         e_prob = save_prob
 
-                    while row_length > 0:
-                        old_tag = pos_array[pos_len - row_length]
+                    for idx in range(pos_len):
+                        old_tag = pos_array[idx]
                         t_prob = read_files.get_tprob(pos_array[row], old_tag)
-                        print(t_prob)
                         if t_prob == 0:
                             t_prob = save_prob
 
-                        temp_prob = mat[pos_len - row_length, col_prev] * abs(np.log(float(e_prob)) + np.log(float(t_prob)))
+                        temp_prob = mat[col_prev, idx] * abs(np.log(float(e_prob)) + np.log(float(t_prob)))
 
-                        if temp_prob > mat[row, col]:
-                            mat[row, col] = temp_prob
+                        if temp_prob > mat[col, row]:
+                            mat[col, row] = temp_prob
                             backtrace[col] = old_tag
 
-                        row_length -= 1
 
         for i in range(backtrace.__len__()):
             print(token_arr[i] + space + backtrace[i])
