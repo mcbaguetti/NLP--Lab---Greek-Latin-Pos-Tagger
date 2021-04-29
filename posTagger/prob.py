a4dde098808f775fc38a1989364b36e2d4956314
T. C <comelli.tom@gmail.com>
2021-04-29 15:35:03 +0200

    create prob and updated viterbi
    prob joined some functions

--- posTagger/prob.py
+++ posTagger/prob.py
@@ -1,15 +1,18 @@
 import pyconll
 from pathlib import Path
+import numpy as np
 
 sos = 'SoS'
 eos = 'EoS'
+n_tag = 17
 
 space = " "
 equal = " = "
 arrow = " -> "
 slash = " / "
+new_line = "\n"
 
-countpos = {'SoS':1, 'EoS':1}
+countpos = {'SoS': 1, 'EoS': 1}
 eprob = {'SoS -> SoS': 1, 'EoS -> EoS': 1}
 tprob = {}
 countname = {}
@@ -18,8 +21,6 @@ frequent_tag_word = {}
 
 file_path = Path("C:/Users/funkt/Documents/GitHub/nlp-greek-latin-pos-tagger/UD_Latin-LLCT-master/la_llct-ud-train.conllu")
 corpus = pyconll.iter_from_file(file_path)
-corpus1 = pyconll.iter_from_file(file_path)
-corpus2 = pyconll.iter_from_file(file_path)
 
 
 #salva i tag e conta le loro occorrenze
@@ -28,14 +29,11 @@ def count_npos(corpus):
     for sentence in corpus:
         for token in sentence:
             add_to_hash(countpos, token.upos)
-
-    countpos.update({"tot_pos": tot})
-
     return countpos
 
 
 #calcola le emission probability e le ritorna tramite dizionario
-def calc_eprob(corpus):
+def e_prob(corpus):
 
     #la prima parte della funzione calcola le occorrenze delle varie combinazioni tag -> parola
     for sentence in corpus:
@@ -46,7 +44,7 @@ def calc_eprob(corpus):
     #divide i value di eprob per il corrispettivo value di countpos, caratterizzato dallo stesso tag
     for name in eprob:
         #salva in una str la prima parte della key di eprob cioè il tag
-        key_pos = name.split(" ", 1)[0]
+        key_pos = name.split(space, 1)[0]
         if key_pos in n_pos.keys():
             eprob[name] /= n_pos[key_pos]
 
@@ -69,6 +67,7 @@ def t_prob(corpus):
     for key in tprob:
         tprob[key]/= num_sos
 
+def t_prob_fin(corpus):
     num_eos = 0
     # calcolo tutte le altre probabilità di SoS->tag
     for sentence in corpus:
@@ -90,8 +89,8 @@ def t_prob(corpus):
     #calcola tutte le altre probabilità di trasmissione tranne sos->tag
     for name in tprob:
         # salva in una str la seconda parte della key di tprob cioè il tag dopo la freccia
-        key_eos = name.split("-> ", 1)[1]
-        key_sos = name.split(" ->", 1)[0]
+        key_eos = name.split(arrow, 1)[1]
+        key_sos = name.split(arrow, 1)[0]
         if key_eos == eos:
             tprob[name] /= num_eos
 
@@ -136,18 +135,28 @@ def most_used_tag(corpus):
             key_copy_name = copy.split(slash, 1)[0]
             if key_copy_name == key_name and cprob[name] >= copy_dict[copy]:
                 frequent_tag_word.update({key_name : key_pos})
-
     return frequent_tag_word
+
+
+#ritorna un array di tutti i tags
+def get_tags():
+    pos_array = np.empty(shape=n_tag, dtype=np.dtype('U5'))
+    count = 0
+
+    for name in countpos:
+        tag = name.split(new_line, 1)[0]
+        pos_array[count] = tag
+        count += 1
+
+    return pos_array
+
 
 #aggiunge una una chiave ad una hash oppure se esiste già incrementa il suo valore
 def add_to_hash(hash, key):
     if key not in hash.keys():
         hash.update({key: 1})
-
     else:
         hash[key] += 1
 
 
-n_pos = count_npos(corpus2)
-calc_eprob(corpus1)
-t_prob(corpus)
+n_pos = count_npos(corpus)
--- posTagger/viterbiAlg.py
+++ posTagger/viterbiAlg.py
@@ -1,57 +1,62 @@
 import pyconll.util
 from pathlib import Path
 import numpy as np
-import read_files
-import emissionProb
-import transitionProb
+import prob
 import accuracyViterbi
 
+
 file_pathdev = Path("C:/Users/funkt/Documents/GitHub/nlp-greek-latin-pos-tagger/UD_Latin-LLCT-master/la_llct-ud-train.conllu")
-file_pathdev1 = Path("C:/Users/funkt/Documents/GitHub/nlp-greek-latin-pos-tagger/UD_Latin-LLCT-master/la_llct-ud-test.conllu")
+file_pathdev1 = Path("C:/Users/funkt/Documents/GitHub/nlp-greek-latin-pos-tagger/UD_Latin-LLCT-master/la_llct-ud-dev.conllu")
 corpus1 = pyconll.iter_from_file(file_pathdev1)
 corpus = pyconll.iter_from_file(file_pathdev)
 corpus2 = pyconll.iter_from_file(file_pathdev)
+corpus3 = pyconll.iter_from_file(file_pathdev)
 
+#sono i caratteri SoS + EoS
+special_char = 2
 save_prob = 0.00000000001
+space = " "
 arrow = " -> "
 sos = "SoS"
 eos = "EoS"
-#sono i caratteri SoS + EoS
-special_char = 2
-space = " "
-pos_array = read_files.get_tags()
+pos_array = prob.get_tags()
 pos_len = pos_array.__len__()
-eprob = emissionProb.calc_eprob(corpus)
-tprob = transitionProb.tprob2(corpus2)
+eprob = prob.e_prob(corpus)
+prob.t_prob(corpus2)
+tprob = prob.t_prob_fin(corpus3)
 
 
+#se non trova la probabilità ritorna la save_prob altrimenti ritorna quella corretta
 def get_eprob(pos, token):
     name = pos + arrow + token
     if name not in eprob:
         return save_prob
     return eprob[name]
 
+
+#se non trova la probabilità ritorna la save_prob altrimenti ritorna quella corretta
 def get_tprob(tag, oldtag):
     name = oldtag + arrow + tag
     if name not in tprob:
         return save_prob
     return tprob[name]
 
+
+#usa la matrice di viterbi per calcolare i pos
 def viterbi(corpus):
 
     for sentence in corpus:
 
-        print(sentence.id)
         s_length = sentence.__len__()
         mat = np.zeros(shape=(pos_len, s_length + special_char))
-        token_arr = np.empty(shape=(s_length + special_char), dtype=np.dtype('U15'))
+        token_arr = np.empty(shape=(s_length + special_char), dtype=np.dtype('U20'))
         count = 0
         token_arr[count] = sos
-        backtrace = np.empty(shape=(s_length + special_char), dtype=np.dtype('U15'))
+        backtrace = np.empty(shape=(s_length + special_char), dtype=np.dtype('U5'))
         backtrace[0] = sos
         max_col = 1
         #corrisponde a sos nel tagset, quindi è il valora iniziale
-        index_max_col = 15
+        index_max_col = 0
 
 
         #inizializzo l'array di token
@@ -67,6 +72,7 @@ def viterbi(corpus):
                 if col == 0:
                     e_prob = get_eprob(pos_array[row], token_arr[col])
                     mat[row, col] = e_prob
+
 
                 #per le successive colonne controllo le righe della colonna precedente e salvo la massima prob per ogni casella della colonna attuale
                 else:
@@ -87,7 +93,7 @@ def viterbi(corpus):
             print(token_arr[i] + space + backtrace[i])
 
         accuracyViterbi.save_num(backtrace, sentence)
-
     accuracyViterbi.print_accuracy()
+
 
 viterbi(corpus1)
